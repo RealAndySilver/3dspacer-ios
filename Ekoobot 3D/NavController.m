9a46fe6c39f69523cf398c0eb0e2c43788c3d537
Andres Abril <andres.abril@gmail.com>
2012-09-25 17:01:34 -0500

    Autorotate fixed in iOS6 adding a new Navigation Controller subclass

---
+++ Ekoobot 3D/NavController.m
@@ -0,0 +1,38 @@
+//
+//  NavController.m
+//  Ekoobot 3D
+//
+//  Created by Andrés Abril on 25/09/12.
+//  Copyright (c) 2012 Ekoomedia. All rights reserved.
+//
+
+#import "NavController.h"
+
+@interface NavController ()
+
+@end
+
+@implementation NavController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

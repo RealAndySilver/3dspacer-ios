b81452c8779ac56d463e13eb1a228ac13491a0cf
Andres Abril <andres.abril@gmail.com>
2012-12-11 17:54:15 -0500

    - Muchos cambios

---
+++ BrujulaView.h
@@ -0,0 +1,13 @@
+//
+//  BrujulaView.h
+//  Ekoobot 3D
+//
+//  Created by Andres Abril on 11/12/12.
+//  Copyright (c) 2012 Ekoomedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrujulaView : UIView
+
+@end
---
+++ BrujulaView.m
@@ -0,0 +1,31 @@
+//
+//  BrujulaView.m
+//  Ekoobot 3D
+//
+//  Created by Andres Abril on 11/12/12.
+//  Copyright (c) 2012 Ekoomedia. All rights reserved.
+//
+
+#import "BrujulaView.h"
+
+@implementation BrujulaView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
